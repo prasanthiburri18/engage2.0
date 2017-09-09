fcca3a52b0560934400c5a40dedbb05bf0230d17
svprudhvi13 <svprudhvi13@yahoo.com>
2017-09-09 17:24:37 +0530

    changed db scripts

--- engage-db/encrypt_table.sql
+++ engage-db/encrypt_table.sql
@@ -1,6 +1,6 @@
 
-DROP TABLE IF EXISTS `qc_encrypt_key`;
-CREATE TABLE `qc_encrypt_key` (
+DROP TABLE IF EXISTS `patientdb`.`qc_encrypt_key`;
+CREATE TABLE `patientdb`.`qc_encrypt_key` (
   `id` int(11) NOT NULL,
   `key` varchar(50) NOT NULL,
   `is_active` char NOT NULL,
@@ -8,5 +8,18 @@ CREATE TABLE `qc_encrypt_key` (
   UNIQUE KEY  (`id`, `key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-insert into qc_encrypt_key(id, `key`,`is_active`) values(1,"Secure@helthapi1", 'Y');
+insert into `patientdb`.qc_encrypt_key(id, `key`,`is_active`) values(1,"Secure@helthapi1", 'Y');
+commit;
+
+
+DROP TABLE IF EXISTS `schedulerdb`.`qc_encrypt_key`;
+CREATE TABLE `schedulerdb`.`qc_encrypt_key` (
+  `id` int(11) NOT NULL,
+  `key` varchar(50) NOT NULL,
+  `is_active` char NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY  (`id`, `key`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+insert into `schedulerdb`.qc_encrypt_key(id, `key`,`is_active`) values(1,"Secure@helthapi1", 'Y');
 commit;
