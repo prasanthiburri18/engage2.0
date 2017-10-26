556a5bf4662f47aa9d00cea2492a9c09597e8eee
svprudhvi13 <svprudhvi13@yahoo.com>
2017-10-26 20:15:08 +0530

    Team list bug fix, Host Filter check

--- ApiGateway/src/main/java/com/engage/HostFilter.java
+++ ApiGateway/src/main/java/com/engage/HostFilter.java
@@ -55,6 +55,14 @@ public class HostFilter extends GenericFilterBean {
 			throws IOException, ServletException {
 		HttpServletRequest req = (HttpServletRequest) request;
 		final String host = req.getHeader("Host");
+		final String customHeader = req.getHeader("X-Requested-With");
+		LOGGER.info("custom header "+customHeader);
+		
+		if(customHeader==null ||!customHeader.equalsIgnoreCase("XMLHttpRequest")){
+			LOGGER.error("Not an ajax call");
+			((HttpServletResponse)response).sendError(HttpServletResponse.SC_PRECONDITION_FAILED);
+			
+		}
 		try{
 			final FileInputStream blackListFile = new FileInputStream(
 					RefererFilter.class.getClassLoader().getResource("host.properties").getFile());
